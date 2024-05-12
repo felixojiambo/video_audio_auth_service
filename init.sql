d62adb53d7421e95c087c973de25d8d81d076527
Felix Ojiambo <felixojiamboe@gmail.com>
2024-05-12 15:14:53 -0700

    test user creation

---
+++ init.sql
@@ -0,0 +1,13 @@
+-- CREATE USER 'auth_user'@'localhost' IDENTIFIED BY 'Auth123';
+
+-- CREATE DATABASE AuthService;
+-- GRANT ALL PRIVILEGES ON Auth.* TO 'auth_user'@'localhost';
+
+-- USE AuthService;
+
+-- CREATE TABLE `user` (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     email VARCHAR(255) NOT NULL,
+--     password VARCHAR(255) NOT NULL
+-- );
+-- INSERT INTO `user` (email, password) VALUES ('ojiamboloc@gmail.com', 'Admin123');
