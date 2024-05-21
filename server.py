4d412711d4f5c8e640854ab6fa83d7f1a428fd67
Felix Ojiambo <felixojiamboe@gmail.com>
2024-05-20 23:26:02 -0700

    create jwt on login auth

--- server.py
+++ server.py
@@ -5,10 +5,10 @@ server=Flask(__name__)
 mysql=MySQL(server)
 #config 
 server.config["MYSQL_HOST"]=os.environ.get("MYSQL_HOST", "localhost")
-server.config["MYSQL_USER"]=os.environ.get("MYSQL_USER")
-server.config["MYSQL_PASSWORD"]=os.environ.get("MYSQL_PASSWORD")
-server.config["MYSQL_DB"]=os.environ.get("MYSQL_DB")
-server.config["MYSQL_PORT"]=os.environ.get("MYSQL_PORT")
+server.config["MYSQL_USER"]=os.environ.get("MYSQL_USER","root")
+server.config["MYSQL_PASSWORD"]=os.environ.get("MYSQL_PASSWORD","5418feliX")
+server.config["MYSQL_DB"]=os.environ.get("MYSQL_DB","AuthService")
+server.config["MYSQL_PORT"]=os.environ.get("MYSQL_PORT",3308)
 
 @server.route("/login",methods=["POST"])
 def login():
@@ -31,3 +31,14 @@ def login():
              return createJWT(auth.username,os.environ.get("JWT_SECRET"),True)
     else:
         return "invalids creditials",401      
+def createJWT(username,secret,authz):
+     return jwt.encode(
+          {"username":username,
+          "exp":datetime.datetime.now(tz=datetime.timezone.UTC)
+          +datetime.timedelta(day=1),
+          "iat":datetime.datetime.utcnow(),
+          "admin":authz,
+          },
+          secret,
+          algorithm="HS256",
+     )
