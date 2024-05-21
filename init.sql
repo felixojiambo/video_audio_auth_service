96537cde75f24e08545d55e7c15f561d04d9a7ec
Felix Ojiambo <felixojiamboe@gmail.com>
2024-05-21 00:25:07 -0700

    requirements.txt

--- init.sql
+++ init.sql
@@ -1,19 +1,19 @@
-CREATE USER 'auth_user'@'localhost' IDENTIFIED BY 'Auth123';
+-- CREATE USER 'auth_user'@'localhost'  IDENTIFIED BY 'Auth123';
 
-CREATE DATABASE AuthService;
-GRANT ALL PRIVILEGES ON AuthService.* TO 'auth_user'@'localhost';
+-- CREATE DATABASE AuthService;
+-- GRANT ALL PRIVILEGES ON AuthService.* TO 'auth_user'@'localhost';
 
-USE AuthService;
+-- USE AuthService;
 
-CREATE TABLE `user` (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
-);
+-- CREATE TABLE `user` (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     email VARCHAR(255) NOT NULL UNIQUE,
+--     password VARCHAR(255) NOT NULL
+-- );
 
-INSERT INTO `user` (email, password) VALUES ('ojiamboloc@gmail.com', 'Admin123');
+-- INSERT INTO `user` (email, password) VALUES ('ojiamboloc@gmail.com', 'Admin123');
 
 
--- //basic authentication
--- username
--- password
+-- -- //basic authentication
+-- -- username
+-- -- password
